3024dc4549831fdfd04e0a67ef88be3d31949582
binz.zhang <ligongzhangbin@163.com>
2019-12-02 20:25:29 +0800

    update commons-compress to 1.19

--- src/main/java/com/qunar/qfproxy/utils/XMLUtil.java
+++ src/main/java/com/qunar/qfproxy/utils/XMLUtil.java
@@ -4,10 +4,14 @@ import com.qunar.qfproxy.constants.PackageXMLConstant;
 import com.qunar.qfproxy.model.EmoPackXML;
 import org.dom4j.Document;
 import org.dom4j.DocumentHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 public class XMLUtil {
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmoPackageUtil.class);
+
     public static Document dom4jToXml(EmoPackXML emoPackXML) {
         EmoPackXML.FACESETTINGBean.DEFAULTFACEBean facesettingBean = emoPackXML.getFACESETTING().getDEFAULTFACE();
         Document document = DocumentHelper.createDocument();
@@ -52,4 +56,26 @@ public class XMLUtil {
         return document;
     }
 
+    public static void main(String[] args) {
+        int valueTen = 128320;
+        //将其转换为十六进制并输出
+        String strHex = Integer.toHexString(valueTen);
+        LOGGER.info(valueTen + " [十进制]---->[十六进制] " + strHex);
+        //将十六进制格式化输出
+        String strHex2 = String.format("%07x",valueTen);
+        LOGGER.info(valueTen + " [十进制]---->[十六进制] " + strHex2);
+
+        LOGGER.info("==========================================================");
+        //定义一个十六进制值
+        String strHex3 = "00001322";
+        //将十六进制转化成十进制
+        int valueTen2 = Integer.parseInt(strHex3,16);
+        LOGGER.info(strHex3 + " [十六进制]---->[十进制] " + valueTen2);
+
+        LOGGER.info("==========================================================");
+        //可以在声明十进制时，自动完成十六进制到十进制的转换
+        int valueHex = 0x00001322;
+        LOGGER.info("int valueHex = 0x00001322 --> " + valueHex);
+
+    }
 }
